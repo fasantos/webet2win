f778dcfea800405de0837b519bb721fc701b5389
fasantos <filomenosantos@gmail.com>
2015-03-29 15:02:18 +0200

    Alignment bug fix

--- app/assets/javascript/directives/b2wCalculator.js
+++ app/assets/javascript/directives/b2wCalculator.js
@@ -2,6 +2,144 @@ angular.module('WeBet2Win').directive('b2wCalculator', function(){
 	return {
 		replace: true,
 		restrict: "E",
-		templateUrl: "assets/templates/directives/b2wCalculator.html"
+		templateUrl: "assets/templates/directives/b2wCalculator.html",
+		controller: function($scope){
+			// Calculator
+			$("betValue").on("keyup", function(){calculator();});
+			$("houseOdd").on("keyup", function(){calculator();});
+			$("freebet").on("change", function(){calculator();});
+			$("betfairOdd").on("keyup", function(){calculator();});
+			$("comission").on("keyup", function(){calculator();});
+
+			$scope.calculator = function(){
+				var betValue = Number($("betValue").value);
+			 	var houseOdd = Number($("houseOdd").value);
+			 	var freebet = $("freebet").checked;
+			 	var betfairOdd = Number($("betfairOdd").value);
+			 	var comission = Number($("comission").value);
+			 	var result;
+
+			 	//alert(betValue);
+			 	if(isNaN(betValue) || isNaN(houseOdd) || isNaN(betfairOdd) || isNaN(comission)){
+					alert("Please enter a numeric value.");
+				}else{
+					if(freebet){
+			 			result = calculateWithFreebet(betValue, houseOdd, betfairOdd, comission);
+			 		}else{
+			 			result = calculateWithoutFreebet(betValue, houseOdd, betfairOdd, comission);
+			 		}
+				}
+			 	
+
+			 	$("val2Bet").value = Math.round(result[0] * 100) / 100;
+				$("balNeeded").value = Math.round(result[1] * 100) / 100;
+				$("winbetfair").value = Math.round(result[2] * 100) / 100;
+				$("winbethouse").value = Math.round(result[2] * 100) / 100;
+
+			}
+
+			$scope.calculateWithFreebet = function(betValue, houseOdd, betfairOdd, comission){
+				var valToBet = ((houseOdd - 1)/ (betfairOdd - (comission / 100))) * betValue;
+				var balNeeded = (betfairOdd - 1) * valToBet;
+				var win = ((houseOdd - 1) * betValue) - balNeeded;
+				return [valToBet, balNeeded, win]
+			}
+
+			$scope.calculateWithoutFreebet = function(betValue, houseOdd, betfairOdd, comission){
+				var valToBet = (houseOdd / (betfairOdd - (comission / 100))) * betValue;
+				var balNeeded = (betfairOdd - 1) * valToBet;
+				var win = ((houseOdd - 1) * betValue) - balNeeded;
+				return [valToBet, balNeeded, win]
+			}
+
+			//Odds Converter
+			$("quocient").on("keyup", function(){fractional();});
+			$("dividend").on("keyup", function(){
+				auxfunc();
+			});
+			$("signal").on("keyup", function(){american();});
+			$("signal").on("change", function(){american();});
+			$("aDividend").on("keyup", function(){american();});
+
+			$scope.auxfunc = function(){
+				var dividend = Number($("dividend").value);
+				var sDividend = $("dividend").value;
+				if(sDividend !== ""){
+					if(dividend === 0){
+						alert("Dividend must be diferent then 0. ");
+					}else{
+						fractional();
+					}
+				}
+			}
+
+			$scope.fractional = function(){
+				var quocient = Number($("quocient").value);
+			 	var dividend = Number($("dividend").value);
+
+			 	if(isNaN(quocient) || isNaN(dividend)){
+					alert("Please enter a numeric value.");
+				}else{
+			 		$("fractRsul").value = Math.round((quocient / dividend) + 1 * 100) / 100;
+			 	}
+			}
+
+			$scope.american = function(){
+			 	var signal = $("signal").value;
+			 	var aDividend = Number($("aDividend").value);
+
+			 	if (signal !== "+" && signal !== "-"){
+			 		alert("Signal must be '+' or '-'.");
+			 	}else if (isNaN(aDividend)){
+			 		alert("Please enter a numeric value.");
+			 	}else if (signal === "+"){
+			        $("amRsul").value = Math.round(aDividend / 100 + 1 * 100) / 100;
+			    }
+			    else{
+			       	$("amRsul").value = Math.round(100 / aDividend + 1 * 100) / 100;
+			    }
+			}
+
+			//Ajax currency converter handler
+			google.load("jquery", "1.2.6");function gp_currencySymbols() {
+				$.getJSON("http://www.geoplugin.net/currency_symbols.gp?format=json&jsoncallback=?",
+					function(data){
+						var currencyCode = geoplugin_currencyCode();
+						var fromCurr = '';
+						var toCurr = '';
+
+						$.each(data, function(i,item){
+							if ( currencyCode == i ) {
+								fromCurr = fromCurr + "<option value='"+i+"' selected>"+item.name+" "+(item.symbol?item.symbol:'')+"</option>";
+							} else {
+								fromCurr = fromCurr + "<option value='"+i+"'>"+item.name+" "+(item.symbol?item.symbol:'')+"</option>";
+							}
+						});          
+						$.each(data, function(i,item){
+							if ( currencyCode == 'USD' && i == 'EUR' ) {
+								toCurr = toCurr + "<option value='"+i+"' selected>"+item.name+" "+(item.symbol?item.symbol:'')+"</option>";
+							} else {
+								toCurr = toCurr + "<option value='"+i+"'>"+item.name+" "+(item.symbol?item.symbol:'')+"</option>";
+							}
+						});
+						$(fromCurr).appendTo("#gp_from");
+						$(toCurr).appendTo("#gp_to");
+					});
+			};
+			function gp_convertIt() {
+				if (!$('gp_amount').value){
+					return false;
+				} else {
+				var gp_from = $('gp_from').value;
+				var gp_to = $('gp_to').value;
+				var gp_amount = $('gp_amount').value;
+				//Ajax currency converter callback
+				$.getJSON( "http://www.geoplugin.net/currency_converter.gp?jsoncallback=?", { from:gp_from, to:gp_to, amount:gp_amount },
+					function(output){
+						$("gp_converted").value = output.to.amount;
+					});
+				}
+			}
+		}
 	};
 });
